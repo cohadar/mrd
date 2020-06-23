6c880025ca17931b991d7e9f29f020161d64ead8
Damir Čohadarević <cohadar@gmail.com>
2020-06-23 13:08:32 +0200

    lupa

--- код/мрд/шкработине.py
+++ код/мрд/шкработине.py
@@ -103,3 +103,31 @@ class ШаренаПозадина():
         for _ in range(10000):
             бре.пиксели[random.randint(0, бре.ширина * бре.висина * 4 - 1)] = random.randint(0, 255)
 
+
+class Лупа():
+    def обради(бре, догађај):
+        код = догађај.key.keysym.scancode
+        if код == sdl2.SDL_SCANCODE_L:
+            pass
+
+    def __init__(бре, главна_површ, обрада_догађаја):
+        print("Лупа __init__")
+        обрада_догађаја.региструј(sdl2.SDL_KEYDOWN, бре.обради)
+        бре.главна_површ = главна_површ
+        бре.извор = sdl2.SDL_Rect(0, 0, 16, 16)
+        бре.притока = sdl2.SDL_Rect(16, 16, 16*4, 16*4)
+        бре.фокус = sdl2.SDL_CreateRGBSurfaceWithFormat(0, 16, 16, 32, sdl2.SDL_PIXELFORMAT_RGBA32)
+        if not бре.фокус:
+            raise Exception('SDL_CreateRGBSurfaceWithFormat', sdl2.SDL_GetError())
+        рез = sdl2.SDL_SetSurfaceBlendMode(бре.фокус, sdl2.SDL_BLENDMODE_NONE)
+        if рез != 0:
+            raise Exception('SDL_SetSurfaceBlendMode', sdl2.SDL_GetError())
+
+    def нашкрабај(бре):
+        рез = sdl2.SDL_BlitSurface(бре.главна_површ, бре.извор, бре.фокус, бре.извор)
+        if рез != 0:
+            raise Exception('SDL_BlitSurface', sdl2.SDL_GetError())
+        рез = sdl2.SDL_BlitScaled(бре.фокус, бре.извор, бре.главна_површ, бре.притока)
+        if рез != 0:
+            raise Exception('SDL_BlitSurface', sdl2.SDL_GetError())
+
