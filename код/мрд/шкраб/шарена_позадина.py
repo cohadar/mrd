6595e0e537295d2502140ee07a080889cb6a1090
Damir Čohadarević <cohadar@gmail.com>
2020-06-27 23:57:35 +0200

    meh

--- код/мрд/шкраб/шарена_позадина.py
+++ код/мрд/шкраб/шарена_позадина.py
@@ -1,21 +1,28 @@
+import sdl2
 import ctypes
 import random
+from мрд.дуњалук import Површ
 
 
 class ШаренаПозадина():
+    """ пример директне манипулације пикселима на површи """
     def __init__(бре, површ, шкработине):
         бре.површ = површ
+        бре.шарено = Површ(бре.површ.contents.w, бре.површ.contents.h, sdl2.SDL_BLENDMODE_NONE)
+        бре.корак = бре.шарено.contents.pitch
+        ПА = ctypes.c_ubyte * (бре.шарено.contents.h * бре.корак)
+        бре.пиксели = ПА.from_address(бре.шарено.contents.pixels)
+        бре.висина = бре.шарено.contents.h
+        бре.ширина = бре.шарено.contents.w
         бре.шкработине = шкработине
         шкработине.додај(0, бре)
 
     def нашкрабај(бре):
-        корак = бре.површ.contents.pitch
-        ПА = ctypes.c_ubyte * (бре.површ.contents.h * корак)
-        пиксели = ПА.from_address(бре.површ.contents.pixels)
-        висина = бре.површ.contents.h
-        ширина = бре.површ.contents.w
-        for _ in range(1000):
-            y = random.randint(0, висина - 1)
-            x = random.randint(0, ширина - 1)
-            индекс = y * корак + x * 4
-            пиксели[индекс + random.randint(0, 3)] = random.randint(0, 255)
+        for _ in range(100):
+            y = random.randint(0, бре.висина - 1)
+            x = random.randint(0, бре.ширина - 1)
+            индекс = y * бре.корак + x * 4
+            бре.пиксели[индекс + random.randint(0, 3)] = random.randint(0, 255)
+        рез = sdl2.SDL_BlitSurface(бре.шарено, None, бре.површ, None)
+        if рез != 0:
+            raise Exception('SDL_BlitSurface', sdl2.SDL_GetError())
