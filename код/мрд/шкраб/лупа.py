16ebbb9a5148ca773ee1d7083de227cf878c006c
Damir Čohadarević <cohadar@gmail.com>
2020-06-27 18:12:13 +0200

    meh

--- код/мрд/шкраб/лупа.py
+++ код/мрд/шкраб/лупа.py
@@ -7,10 +7,11 @@ class Лупа():
         if код == sdl2.SDL_SCANCODE_L:
             pass
 
-    def __init__(бре, главна_површ, обрада_догађаја, шкработине):
+    def __init__(бре, главна_површ, стаклена_површ, обрада_догађаја, шкработине):
         print("Лупа __init__")
         обрада_догађаја.региструј(sdl2.SDL_KEYDOWN, бре.обради)
         бре.главна_површ = главна_површ
+        бре.стаклена_површ = стаклена_површ
         бре.извор = sdl2.SDL_Rect(0, 0, 16, 16)
         бре.притока = sdl2.SDL_Rect(16, 16, 16*4, 16*4)
         бре.фокус = sdl2.SDL_CreateRGBSurfaceWithFormat(0, 16, 16, 32, sdl2.SDL_PIXELFORMAT_RGBA32)
@@ -26,7 +27,7 @@ class Лупа():
         рез = sdl2.SDL_BlitSurface(бре.главна_површ, бре.извор, бре.фокус, бре.извор)
         if рез != 0:
             raise Exception('SDL_BlitSurface', sdl2.SDL_GetError())
-        рез = sdl2.SDL_BlitScaled(бре.фокус, бре.извор, бре.главна_површ, бре.притока)
+        рез = sdl2.SDL_BlitScaled(бре.фокус, бре.извор, бре.стаклена_површ, бре.притока)
         if рез != 0:
             raise Exception('SDL_BlitSurface', sdl2.SDL_GetError())
 
